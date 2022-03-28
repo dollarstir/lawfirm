9d367bb42f5fa71cf61e4505fb43eecf3c7e202a
dollarstir <kpin463@gmail.com>
2022-03-28 05:20:35 +0000

    frm

---
+++ engine.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'loader/autoloader.php';
+$router = new Router([
+    new Route(
+        '/',
+        function ($context) {
+            return Viewer::view('/main.php', $context);
+        }
+    ),
+    new Route(
+        '/login1',
+        function ($context) {
+            return Viewer::view('/widgets/login/login.php', $context);
+        }
+    ),
+]);
+$router->launch();
