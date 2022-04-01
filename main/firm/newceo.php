0bf53629217ca73b2e13a465174d953eacee7193
dollarstir <kpin463@gmail.com>
2022-04-01 15:12:42 +0000

    dsfsdfsd

--- main/firm/newceo.php
+++ main/firm/newceo.php
@@ -4,7 +4,7 @@
 
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-<title>Finance</title>
+<title>Edit CEO</title>
 
 
 <link rel="stylesheet" href="main/firm/css/bootstrap.min.css" />
