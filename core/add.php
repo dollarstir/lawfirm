d87536469ab6b52370d81a5db34f9448cdeda386
dollarstir <kpin463@gmail.com>
2022-04-04 06:02:45 +0000

    dscsd

--- core/add.php
+++ core/add.php
@@ -4,10 +4,28 @@ class Add extends database
 {
     public $uploaddef = __DIR__.'/../yolkassets/upload';
 
+    public static function rebase($path = '')
+    {
+        if (strpos($path, 'http') || strpos($path, 'www.')) {
+            return $path;
+        }
+
+        $root = explode('/core', __DIR__)[0];
+        $root = array_reverse(explode('/', $root))[0];
+
+        $uri = $_SERVER['REQUEST_URI'];
+
+        if (!preg_match("/^\/?$root/", $uri)) {
+            return "/$path";
+        }
+
+        return "/$root/$path";
+    }
+
     public function insert($table, $records, $files = null, $uploadto = '')
     {
         if ($uploadto == '') {
-            $uploadto = $this->uploaddef;
+            $uploadto = $this->rebase('yolkassets/upload');
         }
         $man = '';
         $vv = '';
