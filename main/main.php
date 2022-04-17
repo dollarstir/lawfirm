7ce29e78b26965fb7295bda058eb3daafa6f80e8
dollarstir <kpin463@gmail.com>
2022-04-17 08:52:42 +0000

    ssasa

--- main/main.php
+++ main/main.php
@@ -10,7 +10,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- favicon
 		============================================ -->
-        <link rel="shortcut icon" type="image/x-icon" href="main/img/favicon.png">
+        <link rel="shortcut icon" href="main/img/favicon.ico" type="image/x-icon">
+<link rel="icon" href="main/img/favicon.ico" type="image/x-icon">
+
         <!-- Bootstrap CSS
 		============================================ -->
         <link rel="stylesheet" href="main/css/bootstrap.min.css">
